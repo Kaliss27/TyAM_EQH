74a30511587a330f5b34dad7fef4bce97454bf87
Karen Peralta <karenlisspelu10@outlook.com>
2020-12-06 22:47:47 -0600

    update

--- AppFB/app/src/main/java/com/example/appfb/MainActivity.java
+++ AppFB/app/src/main/java/com/example/appfb/MainActivity.java
@@ -2,14 +2,20 @@ package com.example.appfb;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth auth;
@@ -19,15 +25,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
 
-        ActionBar actionBar = getSupportActionBar ();
-        if (actionBar != null) {
-            actionBar.setTitle ("App");
-        }
+        Toolbar toolbar = findViewById (R.id.toolbar);
+        setActionBar (Objects.requireNonNull (toolbar));
 
         getSupportFragmentManager()
                 .beginTransaction()
                 .add(R.id.rootContainer, new LoginFrg())
                 .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater ().inflate (R.menu.menu_ab, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId ()) {
+            case R.id.mnuUser:
+                Intent intent = new Intent(this, UserRegister.class);
+                startActivity (intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
