4b433cbe96f59523ed5c18f54d7cdaed5a5df05e
Gabriela Portugal <71885932+Gabby-Portugal@users.noreply.github.com>
2020-12-08 11:29:29 -0600

    AppFB

---
+++ AppFB/app/src/main/java/com/example/appfb/UserRegister.java
@@ -0,0 +1,43 @@
+package com.example.appfb;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.Objects;
+
+public class UserRegister extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_user_register);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setActionBar (Objects.requireNonNull (myToolbar));
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater ().inflate (R.menu.menu_ab, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId ()) {
+            case R.id.mnuUser:
+                Intent intent = new Intent(this, UserRegister.class);
+                startActivity (intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
