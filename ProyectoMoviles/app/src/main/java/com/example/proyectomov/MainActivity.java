912c2581b539c55f9c2a0eac7a7ca51a23f41f79
Gabriela Portugal <71885932+Gabby-Portugal@users.noreply.github.com>
2021-01-20 17:59:27 -0600

    sensor_luz

--- ProyectoMoviles/app/src/main/java/com/example/proyectomov/MainActivity.java
+++ ProyectoMoviles/app/src/main/java/com/example/proyectomov/MainActivity.java
@@ -1,23 +1,34 @@
 package com.example.proyectomov;
 
 import android.app.Activity;
+import android.app.Service;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.proyectomov.ui.login.LoginActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
 
+    SensorManager sensorManager;
+    Sensor sensor;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView (R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
+        new Handler().postDelayed(new Runnable()
+        {
             @Override
             public void run()
             {
@@ -25,6 +36,8 @@ public class MainActivity extends Activity {
                 startActivity(intentLI);
             }
         },4000);
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
     }
 
     @Override
@@ -33,20 +46,39 @@ public class MainActivity extends Activity {
 
     }
 
-
     @Override
     protected void onRestart() {
         super.onRestart();
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
+    protected void onPause()
+    {
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onResume()
+    {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        //int brillo = 0;
+        if(event.sensor.getType() == Sensor.TYPE_LIGHT);
+        {
+            //Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, brillo);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
     }
 
     @Override
