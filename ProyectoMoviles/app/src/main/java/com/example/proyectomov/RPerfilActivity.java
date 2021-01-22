3dc296cc0e30fd5b361850310e6938743678003f
Gabriela Portugal <71885932+Gabby-Portugal@users.noreply.github.com>
2021-01-21 23:16:33 -0600

    Update RPerfilActivity.java

--- ProyectoMoviles/app/src/main/java/com/example/proyectomov/RPerfilActivity.java
+++ ProyectoMoviles/app/src/main/java/com/example/proyectomov/RPerfilActivity.java
@@ -1,6 +1,14 @@
 package com.example.proyectomov;
 
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.text.Editable;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -9,6 +17,7 @@ import android.widget.Toast;
 import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,8 +28,12 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 import java.util.Objects;
 
-public class RPerfilActivity extends FragmentActivity
+public class RPerfilActivity extends FragmentActivity implements SensorEventListener
 {
+
+    SensorManager sensorManager;
+    Sensor sensor;
+
     public EditText edNombre;
     public EditText edEstado;
     public EditText edPhone;
@@ -51,6 +64,9 @@ public class RPerfilActivity extends FragmentActivity
             guardarUsuario();
         }
         );
+
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
     }
 
     private void guardarUsuario()
@@ -86,7 +102,48 @@ public class RPerfilActivity extends FragmentActivity
                     Toast.makeText(getBaseContext(), "Datos registrados correctamente", Toast.LENGTH_LONG).show();
                 })
                 .addOnFailureListener(e -> Toast.makeText(getBaseContext(), "Error al registrar los datos " + e.getMessage(), Toast.LENGTH_LONG).show());
-    }   }
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onSensorChanged(SensorEvent event)
+    {
+        if (!Settings.System.canWrite(getApplicationContext()))
+        {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+            startActivity(intent);
+        }
+
+        if (Settings.System.canWrite(getApplicationContext()))
+        {
+            if(event.sensor.getType() == Sensor.TYPE_LIGHT);
+            {
+                int myBrightness = (int) event.values[0];
+                Settings.System.putInt(getApplicationContext().getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, myBrightness);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy)
+    {
+
+    }
+}
 
 class Usuario
 {
