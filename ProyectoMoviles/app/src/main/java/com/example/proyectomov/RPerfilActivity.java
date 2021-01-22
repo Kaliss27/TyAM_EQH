807082f2cfdbddfcccdf482a9c206e223923572d
Karen Peralta <karenlisspelu10@outlook.com>
2021-01-22 01:54:48 -0600

    Carga de Foto en Bitmap

--- ProyectoMoviles/app/src/main/java/com/example/proyectomov/RPerfilActivity.java
+++ ProyectoMoviles/app/src/main/java/com/example/proyectomov/RPerfilActivity.java
@@ -4,7 +4,13 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.text.Editable;
 import android.view.View;
@@ -12,10 +18,13 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
@@ -24,13 +33,16 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Objects;
 
 public class RPerfilActivity extends Activity {
     public static final int REQUEST_CAMERA_OPEN = 4001;
-    public static final int REQUEST_PERMISSION_CAMERA_LOCATION = 3001;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    public static final int REQUEST_PERMISSION_CAMERA = 3001;
 
     public EditText edNombre;
     public EditText edEstado;
@@ -42,6 +54,9 @@ public class RPerfilActivity extends Activity {
     public ImageButton btnGallery;
 
 
+    ImageView iv;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,19 +74,23 @@ public class RPerfilActivity extends Activity {
         cbEsc=findViewById(R.id.checkbox_esc);
         cbInd=findViewById(R.id.checkbox_ind);
 
+        iv=findViewById(R.id.ivSource);
+
         btnTake=findViewById(R.id.imageButtonC);
+
         btnTake.setOnClickListener(v->{
-            int camPermission = checkSelfPermission (Manifest.permission.CAMERA);
-            int coarsePermission = checkSelfPermission (Manifest.permission.ACCESS_COARSE_LOCATION);
 
-            if (camPermission != PackageManager.PERMISSION_GRANTED || coarsePermission != PackageManager.PERMISSION_GRANTED) {
-                requestPermissions (
-                        new String [] { Manifest.permission.CAMERA, Manifest.permission.ACCESS_COARSE_LOCATION },
-                        REQUEST_PERMISSION_CAMERA_LOCATION
+            int perm = checkSelfPermission (Manifest.permission.CAMERA);
+
+            if (perm != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(
+                        new String[]{Manifest.permission.CAMERA},
+                        REQUEST_PERMISSION_CAMERA
                 );
-
                 return;
             }
+
             tomarFotoP();
         });
 
@@ -89,16 +108,52 @@ public class RPerfilActivity extends Activity {
     }
 
     private void tomarFotoP() {
-        Intent intentC = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
-        //startActivityForResult (intentC, REQUEST_CAMERA_OPEN);
-        if (intentC.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult (intentC, REQUEST_CAMERA_OPEN);
+       Intent intentC = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+       startActivityForResult (intentC, REQUEST_CAMERA_OPEN);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSION_CAMERA)
+        {
+            if (grantResults.length > 0 && grantResults [0] == PackageManager.PERMISSION_GRANTED)
+            {
+                tomarFotoP ();
+            }
         }
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CAMERA_OPEN && resultCode == RESULT_OK)
+        {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            iv.setImageBitmap (bitmap);
+        }
+    }
+
+
 
     private void buscarFotoP() {
 
 
+    }
+
+
+    private Bitmap getBitmapFromDrawable (Drawable drble)
+    {
+        if (drble instanceof BitmapDrawable)
+        {
+            return  ((BitmapDrawable) drble).getBitmap ();
+        }
+        Bitmap bitmap = Bitmap.createBitmap (drble.getIntrinsicWidth (), drble.getIntrinsicHeight (), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        drble.setBounds (0, 0, canvas.getWidth (), canvas.getHeight ());
+        drble.draw (canvas);
+        return bitmap;
     }
 
     private void guardarUsuario() {
