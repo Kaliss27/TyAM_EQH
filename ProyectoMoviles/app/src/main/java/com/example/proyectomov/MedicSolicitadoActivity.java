d8f1826385b2a7a6a1a098ac7253bca7ef755d34
Karen Peralta <karenlisspelu10@outlook.com>
2021-01-23 02:20:14 -0600

    Update

---
+++ ProyectoMoviles/app/src/main/java/com/example/proyectomov/MedicSolicitadoActivity.java
@@ -0,0 +1,107 @@
+package com.example.proyectomov;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Spinner;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Marker;
+
+import java.util.Objects;
+
+public class MedicSolicitadoActivity extends FragmentActivity implements OnMapReadyCallback, SensorEventListener {
+
+    SensorManager sensorManager;
+    Sensor sensor;
+    private GoogleMap mMap;
+    private Marker marcador;
+
+    Editable medcName;
+    Spinner spnn;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_medicamentos);
+
+        Toolbar toolbar = findViewById (R.id.toolbar);
+        setActionBar (Objects.requireNonNull (toolbar));
+        toolbar.setNavigationIcon(R.drawable.ic_action_name); //Define icono para toolbar
+
+
+
+
+
+        sensorManager = (SensorManager) getSystemService(Service.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater ().inflate (R.menu.top_app_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent nextAct;
+        switch (item.getItemId())
+        {
+            case R.id.perfil:
+                nextAct= new Intent(this,PerfilActivity.class);
+                startActivity(nextAct);
+                return true;
+
+
+            case R.id.recibidas:
+                nextAct= new Intent(this,RecepcionesActivity.class);
+                startActivity(nextAct);
+                return true;
+
+            case R.id.map:
+                nextAct= new Intent(this,MapsActivity.class);
+                startActivity(nextAct);
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+}
